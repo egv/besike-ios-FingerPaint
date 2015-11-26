32239573f32ce8a22c98bfc483f84fcba7b6c686
Gena <g@goodworkapps.com>
2015-11-26 16:10:55 +0100

    Added framework and shared scheme for it

---
+++ FingerPaintCanvas/FingerPaintCanvas.h
@@ -0,0 +1,19 @@
+//
+//  FingerPaintCanvas.h
+//  FingerPaintCanvas
+//
+//  Created by Gennady Evstratov on 26.11.15.
+//  Copyright © 2015 yrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FingerPaintCanvas.
+FOUNDATION_EXPORT double FingerPaintCanvasVersionNumber;
+
+//! Project version string for FingerPaintCanvas.
+FOUNDATION_EXPORT const unsigned char FingerPaintCanvasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FingerPaintCanvas/PublicHeader.h>
+
+
